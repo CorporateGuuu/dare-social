8544402b9a0c1add03e74560baf6a6bdc7b3f55a
CorporateGuuu <123339761+CorporateGuuu@users.noreply.github.com>
2025-10-07 15:37:34 -0400

    Fix Firebase duplicate app initialization by using single app instance

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,9 @@
-import { initializeApp } from "firebase/app";
+import { getApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions, httpsCallable } from "firebase/functions";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "yourapp.firebaseapp.com",
-  projectId: "yourapp",
-  storageBucket: "yourapp.appspot.com",
-  messagingSenderId: "0000000000",
-  appId: "1:0000000000:web:xxxx"
-};
-
-const app = initializeApp(firebaseConfig);
+const app = getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app);
